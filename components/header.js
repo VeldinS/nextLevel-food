bd2b2b6a825c3106fff393c1426afa5eb05334bb
VeldinS <190302158@student.ius.edu.ba>
2024-04-21 15:20:38 +0200

    /

---
+++ components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <img src="/logo.png" alt="A server surrounded by magic sparkles." />
+            <h1>Welcome to this NextJS Course!</h1>
+            <p>🔥 Let&apos;s get started! 🔥</p>
+        </div>
+    );
+}
+
+export default Header;
